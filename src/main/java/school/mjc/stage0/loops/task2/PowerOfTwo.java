18f12fef1193233615dff48ba98e47d25c19f0f0
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-02-13 15:30:29 +0500

    first try

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int i = 0;
+        int pow = 1;
+        if(power < 0){
+            System.out.println("too much power");
+        } else {
+        while(i <= power){
+            System.out.println(pow);
+            pow = pow * 2;
+            i++;
+        }}
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+        int j = 1;
+        int count = 0;
+        while (i <= printToInclusive){
+            while(j <= i){
+                if(i % j == 0){
+                    count++;
+                }
+                j++;
+            }
+            if(count < 3){
+                System.out.println(i);
+            }
+            count = 0;
+            j = 1;
+            i++;
+        }
     }
 }
